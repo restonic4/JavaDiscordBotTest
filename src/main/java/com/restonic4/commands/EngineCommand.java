2f5af4dc2a8a341e71e46ca476a2d2b7c09c7d44
restonic4 <marcosruizlerma@gmail.com>
2024-09-27 14:04:18 +0200

    Engine command

--- src/main/java/com/restonic4/commands/EngineCommand.java
+++ src/main/java/com/restonic4/commands/EngineCommand.java
@@ -2,6 +2,7 @@ package com.restonic4.commands;
 
 import com.restonic4.registry.CommandObject;
 import com.restonic4.registry.CommandRegistry;
+import com.restonic4.util.GitHubHelper;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
@@ -15,7 +16,8 @@ public class EngineCommand extends CommandObject {
     @Override
     public SlashCommandData create(Guild guild) {
         SlashCommandData command = Commands.slash("engine", "Do something related with the engine")
-                .addOption(OptionType.STRING, "action", "The action to do", true);
+                .addOption(OptionType.STRING, "action", "The action to do", true)
+                .addOption(OptionType.STRING, "argument", "The action's argument", true);
 
         return command;
     }
@@ -23,18 +25,27 @@ public class EngineCommand extends CommandObject {
     @Override
     public void onExecute(SlashCommandInteractionEvent event) {
         OptionMapping actionOption = event.getOption("action");
-        OptionMapping typeOption = event.getOption("type");
+        OptionMapping argOption = event.getOption("argument");
 
-        if (actionOption != null && actionOption.getAsString().equals("info")) {
-            if (typeOption != null && typeOption.getAsString().equals("version")) {
+        if (actionOption == null) {
+            event.reply("Action missing");
+        }
+
+        if (argOption == null) {
+            event.reply("Arugment missing");
+        }
+
+        if (actionOption.getAsString().equals("info")) {
+            if (argOption.getAsString().equals("version")) {
                 event.reply("Latest version: " + getLastEngineVersion()).queue();
             }
         }
-
-        event.reply("Pong").queue();
+        else {
+            event.reply("Invalid action");
+        }
     }
 
     private String getLastEngineVersion() {
-        return "1.0.0";
+        return GitHubHelper.getLatestRelease("restonic4", "CitadelEngine");
     }
 }
